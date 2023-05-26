06a64ecdac836e95a8df18ba0930dbf02d8af8d3
AfonyaGit <nikitoz110@gmail.com>
2023-05-26 17:59:30 +0300

    Gun Setup
    -add guns
    -switch/scroll
    -sunc
    -Shooting, Damage, & Respawning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+using Photon.Realtime;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks, IDamageable
 {
 	[SerializeField] GameObject cameraHolder;
 	[SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce,smoothTIme;
-	
+	[SerializeField] Item[] item;
+
+	int itemIndex;
+	int previousItemIndex = -1;
+
 	float verticalLookRotation;
 	bool grounded;
 	Vector3 smoothMoveVelocity;
@@ -18,15 +24,25 @@ public class PlayerController : MonoBehaviour
 
 	PhotonView PV;
 
+	const float maxHealth = 100f;
+	float currentHealth = maxHealth;
+
+	PlayerManager playerManager;
 	void Awake() 
 	{
-	 rb = GetComponent<Rigidbody>();
-	 PV = GetComponent<PhotonView>();
+		rb = GetComponent<Rigidbody>();
+		PV = GetComponent<PhotonView>();
+
+		playerManager = PhotonView.Find((int)PV.InstantiationData[0]).GetComponent<PlayerManager>();
 	}
 
 	void Start()
 	{
-		if(!PV.IsMine)
+		if(PV.IsMine)
+		{
+			EquipItem(0);
+		}
+		else
 		{
 			Destroy(GetComponentInChildren<Camera>().gameObject);
 			Destroy(rb);
@@ -41,6 +57,42 @@ public class PlayerController : MonoBehaviour
 		Look();
 		Move();
 		//Jump();
+		for (int i = 0; i < item.Length; i++)
+		{
+			if(Input.GetKeyDown((i + 1).ToString()))
+			{
+				EquipItem(i);
+				break;
+			}
+		}
+
+		if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+		{
+			if(itemIndex >= item.Length -1)
+			{
+				EquipItem(0);
+			}
+			else
+			{
+				EquipItem(itemIndex + 1);
+			}
+		}
+		else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+		{
+			if(itemIndex <= 0)
+			{
+				EquipItem(item.Length - 1);
+			}
+			else
+			{
+				EquipItem(itemIndex - 1);
+			}
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			item[itemIndex].Use();
+		}
 	}
 
 	void Look()
@@ -57,10 +109,64 @@ public class PlayerController : MonoBehaviour
 
 		moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTIme);
 	}
+	void EquipItem(int _index)
+	{
+		if(_index == previousItemIndex)
+			return;
+
+		itemIndex = _index;
+
+		item[itemIndex].itemGameObject.SetActive(true);
+
+		if(previousItemIndex != -1)
+		{
+			item[previousItemIndex].itemGameObject.SetActive(false);
+		}
+		previousItemIndex = itemIndex;
+
+		if(PV.IsMine)
+		{
+			Hashtable hash = new Hashtable();
+			hash.Add("itemIndex", itemIndex);
+			PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+		}
+	}
+
+	public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+	{
+		if (!PV.IsMine && targetPlayer == PV.Owner)
+		{
+			EquipItem((int)changedProps["itemIndex"]);
+		}
+	}
 	void FixedUpdate() 
 	{
 		if (!PV.IsMine)
 			return;
 		rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);	
+	}
+
+	public void TakeDamage(float damage)
+	{
+		PV.RPC("RPC_TakeDamage", RpcTarget.All, damage);
+	}
+
+	[PunRPC]
+	void RPC_TakeDamage(float damage)
+	{
+		if(!PV.IsMine)
+			return;
+
+		currentHealth -= damage;
+
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+
+	void Die() 
+	{
+		playerManager.Die();
 	}
 }
